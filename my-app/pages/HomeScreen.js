4ea43fcfa9c752860599175dc3e903f69ce16713
Anna Beatriz <158473512+annabeatriz17@users.noreply.github.com>
2025-03-31 22:31:34 -0300

    finish

--- my-app/pages/HomeScreen.js
+++ my-app/pages/HomeScreen.js
@@ -20,6 +20,7 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         fontWeight: "bold",
+        padding: 20,
         marginTop: 20,
     },
 });
--- my-app/pages/ProfileScreen.js
+++ my-app/pages/ProfileScreen.js
@@ -4,8 +4,8 @@ import { StyleSheet, View, Image, Text } from "react-native";
 export default function ProfileScreen( ) {
     return (
         <View style={styles.container}>
-            <Image source={require("../assets/img/profile.jpg")} style={styles.image} />
             <Text style={styles.title}>Bem vindo ao meu perfil</Text>
+            <Image source={require("../assets/img/profile.jpg")} style={styles.image} />
             <Text style={styles.subtitle}>Nome: Anna Beatriz</Text>
             <Text style={styles.subtitle}>Idade: 17 anos</Text>
             <Text style={styles.subtitle}>Cidade: Valinhos</Text>
@@ -25,13 +25,15 @@ const styles = StyleSheet.create({
         width: 200,
         height: 200,
         marginTop: 20,
+        borderRadius: 100,
     },
     title: {
-        fontSize: 20,
+        fontSize: 30,
         fontWeight: "bold",
         marginTop: 20,
     },
     subtitle: {
+        fontWeight: "bold",
         fontSize: 16,
         marginTop: 10,
     },
